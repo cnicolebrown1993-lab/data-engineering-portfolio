c19f09b2805a1bad8058340a801aacd0a7b9d6c0
Cassandra <cnicolebrown1993@gmail.com>
2026-07-10 13:13:57 -0500

    SQL review

--- Project01-Vertex-Commerce-Warehouse/SQL/daily SQL Concept review.sql
+++ Project01-Vertex-Commerce-Warehouse/SQL/daily SQL Concept review.sql
@@ -75,4 +75,72 @@ where state in ('TX','FL','CA')
 Order by state,
 	LastName;
 
+--7/10/2026 review and practice
+--Show the first name, last name, state, and total amount of every customer that has placed an order. Sort by TotalAmount from highest to lowest.
+--forgot column names
+Select *
+from orders
+
+Select 
+	c.firstname,
+	c.lastname,
+	c.state,
+	o.totalamount
+from customers as c
+inner join orders as o
+	on c.CustomerID = o.CustomerID
+order by o.TotalAmount desc;
+--select every customer who has neber placed an order
+Select
+	c.firstname,
+	c.lastname,
+	c.state,
+from customers as c
+left join orders as o
+	on c.CustomerID=o.CustomerID
+where o.CustomerID is null;
+--Show every state and total sales generate from that state
+select
+	c.state,
+	sum(o.totalamount) as TotalSales
+from customers as c
+left join orders as o
+	on c.CustomerID=o.customerid
+group by c.State
+order by sum(o.totalamount) desc;
+
+--Find customers whose average order is greater than 300
+select
+	c.firstname,
+	c.lastname,
+	c.customerid,
+	avg(o.totalamount) as AverageOrder
+from customers as c
+left join orders as o
+	on c.CustomerID=o.CustomerID
+group by 
+	c.CustomerID,
+	c.FirstName,
+	c.LastName
+having Avg(o.totalamount) >300;
+-- 	"Which states have the highest average customer spending?"
+SELECT
+    CustomerSpending.State,
+    AVG(CustomerSpending.TotalCustomerSpend) AS AverageCustomerSpend
+FROM
+(
+    SELECT
+        c.CustomerID,
+        c.State,
+        SUM(o.TotalAmount) AS TotalCustomerSpend
+    FROM Customers AS c
+    INNER JOIN Orders AS o
+        ON c.CustomerID = o.CustomerID
+    GROUP BY
+        c.CustomerID,
+        c.State
+) AS CustomerSpending
+GROUP BY CustomerSpending.State
+ORDER BY AverageCustomerSpend DESC;
+
 
